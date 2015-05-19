c3b8bb1e47ebe65850e6cb0fd33068edf0fe68df
Mashadar- <pelf005@gmail.com>
2015-05-19 15:02:56 +0200

    added swedish Languagefile

---
+++ firmware/Photoduino/language_SE.h
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Photoduino.
+ *
+ * Photoduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Photoduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Photoduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ // Messages list                                                 "123456789012345"
+const prog_char PROGMEM MSG_LANGUAGE_CODE[] =                     "SE";
+const prog_char PROGMEM MSG_PHOTODUINO_V[] =                      "Photoduino v";
+const prog_char PROGMEM MSG_READY[] =                             "Redo!";
+const prog_char PROGMEM MSG_MAIN_MENU[] =                         "huvudmeny";
+const prog_char PROGMEM MSG_SYSTEM_CONFIG[] =                     "System Config";
+const prog_char PROGMEM MSG_INTERVALOMETER[] =                    "Intervalometer";
+const prog_char PROGMEM MSG_SENSOR_TRIGGER[] =                    "Sensor Trigger";
+const prog_char PROGMEM MSG_CONFIG[] =                            "Config";
+const prog_char PROGMEM MSG_CONFIG_TRIGGER[] =                    "Config Trigger";
+const prog_char PROGMEM MSG_CONFIG_DROPS[] =                      "Config Drops";
+const prog_char PROGMEM MSG_RUN[] =                               "Kör";
+const prog_char PROGMEM MSG_RUNNING[] =                           "Kör...";
+const prog_char PROGMEM MSG_RESET_CONFIG[] =                      "Reset Config";
+const prog_char PROGMEM MSG_AUTOFOCUS_TIME[] =                    "AF tid";
+const prog_char PROGMEM MSG_SHUTTERLAG_TIME[] =                   "Shutterlag Time";
+const prog_char PROGMEM MSG_USE_FLASH1[] =                        "Use Flash1";
+const prog_char PROGMEM MSG_USE_FLASH2[] =                        "Use Flash2";
+const prog_char PROGMEM MSG_FLASH1_PRETIME[] =                    "Flash1 Pretime";
+const prog_char PROGMEM MSG_FLASH2_PRETIME[] =                    "Flash2 Pretime";
+const prog_char PROGMEM MSG_CLOSE_PRETIME[] =                     "Close Pretime";
+const prog_char PROGMEM MSG_INTERVAL_UNITS[] =                    "Interval Units";
+const prog_char PROGMEM MSG_INTERVAL_VALUE[] =                    "Interval Value";
+const prog_char PROGMEM MSG_CYCLES[] =                            "Cykler";
+const prog_char PROGMEM MSG_SENSOR[] =                            "Sensor";
+const prog_char PROGMEM MSG_SENSOR_LIMIT[] =                      "Sensor gräns";
+const prog_char PROGMEM MSG_SENSOR_VALUE[] =                      ">Sensor :";
+const prog_char PROGMEM MSG_LIMIT_VALUE[] =                       ">Limit  :";
+const prog_char PROGMEM MSG_SHOOTING_MODE[] =                     "Shooting Mode";
+const prog_char PROGMEM MSG_INTERCYCLE_TIME[] =                   "Intercycle Time";
+const prog_char PROGMEM MSG_BACKLIGHT[] =                         "Backlight";
+const prog_char PROGMEM MSG_SPEAKER[] =                           "Speaker";
+const prog_char PROGMEM MSG_RESETTING[] =                         "Resetting...";
+const prog_char PROGMEM MSG_ABORTING[] =                          "Aborting...";
+const prog_char PROGMEM MSG_YES[] =                               "Ja";
+const prog_char PROGMEM MSG_NO[] =                                "Nej";
+const prog_char PROGMEM MSG_NONE[] =                              "Ingen";
+const prog_char PROGMEM MSG_UNITS_US[] =                          "us";
+const prog_char PROGMEM MSG_UNITS_MS[] =                          "ms";
+const prog_char PROGMEM MSG_UNITS_SECS[] =                        "Sekunder";
+const prog_char PROGMEM MSG_UNITS_MINS[] =                        "Minuter";
+const prog_char PROGMEM MSG_UNITS_HOURS[] =                       "Timmar";
+const prog_char PROGMEM MSG_UNITS_DAYS[] =                        "DAgar";
+const prog_char PROGMEM MSG_UNITS_CYCLES[] =                      "Cykler";
+const prog_char PROGMEM MSG_UNITS_DROPS[] =                       "Dropar";
+const prog_char PROGMEM MSG_SHOOTINGMODE_NORMAL[] =               "Normal";
+const prog_char PROGMEM MSG_SHOOTINGMODE_PREBULB[] =              "Pre-Bulb Mode";
+const prog_char PROGMEM MSG_SHOOTINGMODE_MIRRORLOCKUP[] =         "Spegellåsning";
+const prog_char PROGMEM MSG_SENSOR_TUNING[] =                     "Sensor Tuning";
+const prog_char PROGMEM MSG_SENSOR_TUNING_VISUAL[] =              "Visuell";
+const prog_char PROGMEM MSG_SENSOR_TUNING_NUMERIC[] =             "Numeric";
+const prog_char PROGMEM MSG_SENSOR_TYPE[] =                       "Sensor Type";
+const prog_char PROGMEM MSG_SENSOR_TYPE_AUDIO[] =                 "Audio";
+const prog_char PROGMEM MSG_SENSOR_TYPE_BARRIER[] =               "Barrier";
+const prog_char PROGMEM MSG_SENSOR_TYPE_SHOCK[] =                 "Shock";
+const prog_char PROGMEM MSG_SENSOR_TYPE_LIGHT[] =                 "Light";
+const prog_char PROGMEM MSG_MIRROR_LOCKUP_TIMEOUT[] =             "Mirror Timeout";
+const prog_char PROGMEM MSG_DEVICE_PORT_TYPE[] =                  "Device Type";
+const prog_char PROGMEM MSG_DEVICE_PORT_TYPE_LASER[] =            "Laser";
+const prog_char PROGMEM MSG_DEVICE_PORT_TYPE_SOLENOID_VALVE[] =   "Solenoid Valve";
+const prog_char PROGMEM MSG_DROPS_COUNT[] =                       "Drops Count";
+const prog_char PROGMEM MSG_DROPS_DURATION[] =                    "Drops Duration";
+const prog_char PROGMEM MSG_DROPS_INTERVAL[] =                    "Drops Interval";
+const prog_char PROGMEM MSG_REMOTE_MODE[] =                       "Remote mode";
+
